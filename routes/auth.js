54f0c0ff1367cd5bfba0e8f0956f17d7510b8544
selvananthan <selvagoogly@gmail.com>
2024-09-14 13:48:16 +0530

    minor

--- routes/auth.js
+++ routes/auth.js
@@ -15,8 +15,8 @@ router.post('/login', async (req, res) => {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
   
-    // const token = jwt.sign({ userId: user._id }, secret, { expiresIn: '1h' });
-    // res.json({ token });
+    const token = jwt.sign({ userId: user._id }, secret, { expiresIn: '1h' });
+    res.json({ token });
   } catch (err) {
     res.status(500).json({ message: 'Error logging in', error: err.message });
   }
--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use('/api/bookings', bookingRoutes);
 app.use('/api/payment/',paymentRoutes)
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://selvagoogly:Mars%401992@cluster0.k3rqn.mongodb.net/hotelbooking?retryWrites=true&w=majority&appName=Cluster0',{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://selvagoogly:Mars%401992@cluster0.k3rqn.mongodb.net/hotelbooking?retryWrites=true&w=majority&appName=Cluster0',{ useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 5000 })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
