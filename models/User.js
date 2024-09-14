9962bca89d0e3cd4388ba86f132652a9a56595a1
selvananthan <selvagoogly@gmail.com>
2024-09-14 13:49:53 +0530

    name change foer collection

--- models/User.js
+++ models/User.js
@@ -20,4 +20,4 @@ userSchema.methods.comparePassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema,'User');
