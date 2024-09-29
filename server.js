b36363a8e1b6de4ce3b07c804ca5c7ba6f7eebf8
selvananthan <selvagoogly@gmail.com>
2024-09-29 20:42:21 +0530

    major

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const bookingRoutes = require('./routes/bookings');
 const paymentRoutes = require('./routes/payment')
 const app = express();
 const port = process.env.PORT ||10000;
+const dburl = process.env.MONGO_URI;
+
 
 // CORS options
 const corsOptions = {
@@ -27,16 +29,27 @@ app.use('/api/hotels', hotelRoutes);
 app.use('/api/reviews', reviewRoutes);
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/payment/',paymentRoutes)
+const connectDb = async () => {
 
+  if (mongoose.connection.readyState === 1) {
+      console.log(' MongoDb instance already connected ');
+      return
+  }
+
+  await mongoose.connect(dburl);
+  console.log(mongoose.connection.readyState, ' Connection State');
+}
+connectDb
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://Selva:Mars%401992@selva.tvmfr.mongodb.net/HotelBooking')
-// mongoose.connect('mongodb+srv://selvagoogly:%3CMars%401992%3E@cluster0.k3rqn.mongodb.net/SiddHotelBooking?retryWrites=true&w=majority&appName=Cluster0')
+// mongoose.connect(dburl)
+// // mongoose.connect('mongodb+srv://selvagoogly:%3CMars%401992%3E@cluster0.k3rqn.mongodb.net/SiddHotelBooking?retryWrites=true&w=majority&appName=Cluster0')
 
-// mongoose.connect('mongodb://localhost:27017/SiddHotelBooking',{ useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 5000 ,ssl: false,})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
+// // mongoose.connect('mongodb://localhost:27017/SiddHotelBooking',{ useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 5000 ,ssl: false,})
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch(err => console.error('Could not connect to MongoDB', err));
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
