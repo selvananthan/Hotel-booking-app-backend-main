ad5182266652534f0d271b4d8348fd179e89696d
selvananthan <selvagoogly@gmail.com>
2024-09-30 12:26:43 +0530

    Changed alot

--- routes/auth.js
+++ routes/auth.js
@@ -2,25 +2,31 @@ const express = require('express');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+
 
 // const secret = 'your_jwt_secret';  // Replace with your actual JWT secret
-
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
   try {
-      const dbResponse = await User.findOne({
-          username,
-          password,
-      });
-      console.log(dbResponse, "dbResponse");
-      if (dbResponse._id) {
-          res.send(username);
+      const dbResponse = await User.findOne({ username });
+      if (!dbResponse) {
+          return res.status(400).send('User not found');
       }
+      
+      const isMatch = await bcrypt.compare(password, dbResponse.password); // Compare hashed password
+      if (!isMatch) {
+          return res.status(400).send('Invalid password');
+      }
+      console.log(dbResponse);
+      
+      res.send(dbResponse);
   } catch (error) {
       console.log(error);
-      res.send("error");
+      res.status(500).send('Server error');
   }
 });
+
 
 router.post('/register', async (req, res) => {
   console.log("register entered");
