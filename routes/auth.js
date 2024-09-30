8bd7400ba19f0c3acda8655a8fca220b8e544e3a
selvananthan <selvagoogly@gmail.com>
2024-09-30 12:38:37 +0530

    minor

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 
 // const secret = 'your_jwt_secret';  // Replace with your actual JWT secret
